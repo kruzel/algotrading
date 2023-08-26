d71d2ea8c923ccc0b465aa12ab7e7dac6563f910
ofer kruzel <okruzel@gmail.com>
2023-08-26 11:13:57 +0300

    fully working CHLtrading

--- CHLpredictor.py
+++ CHLpredictor.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from copy import deepcopy as dc
 
 class CHLpredictor(nn.Module):
 
@@ -27,3 +28,47 @@ class CHLpredictor(nn.Module):
         out = self.fc(out[:, -1, :])
         return out
 
+
+
+def prepare_dataframe_for_lstm_with_date(df, n_steps):
+    df = dc(df)
+    df.dropna(inplace=True)
+
+    df1 = dc(df[['Date', 'Close']])
+    df2 = dc(df[['Date', 'Low']])
+    df3 = dc(df[['Date', 'High']])
+    df4 = dc(df[['Date', 'Volume']])
+
+    df1.set_index('Date', inplace=True)
+    df2.set_index('Date', inplace=True)
+    df3.set_index('Date', inplace=True)
+    df4.set_index('Date', inplace=True)
+
+    for i in range(1, n_steps + 1):
+        df1[f'Close(t-{i})'] = df1['Close'].shift(i)
+        df2[f'Low(t-{i})'] = df2['Low'].shift(i)
+        df3[f'High(t-{i})'] = df3['High'].shift(i)
+        df4[f'Volume(t-{i})'] = df4['Volume'].shift(i)
+
+    df1.dropna(inplace=True)
+    df2.dropna(inplace=True)
+    df3.dropna(inplace=True)
+    df4.dropna(inplace=True)
+
+    return df1, df2, df3, df4
+
+def prepare_dataframe_for_lstm(df, n_steps):
+    df = dc(df)
+
+    df1 = dc(df[['Close']])
+    df2 = dc(df[['Low']])
+    df3 = dc(df[['High']])
+    df4 = dc(df[['Volume']])
+
+    for i in range(1, n_steps + 1):
+        df1[f'Close(t-{i})'] = df1['Close'].shift(i)
+        df2[f'Low(t-{i})'] = df2['Low'].shift(i)
+        df3[f'High(t-{i})'] = df3['High'].shift(i)
+        df4[f'Volume(t-{i})'] = df4['Volume'].shift(i)
+
+    return df1, df2, df3, df4
