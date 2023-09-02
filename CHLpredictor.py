35e7deac6903404e523996b2fd37dd73f48700cd
ofer kruzel <okruzel@gmail.com>
2023-09-02 20:44:08 +0300

    change to close, Low Change, High Change prediction

--- CHLpredictor.py
+++ CHLpredictor.py
@@ -10,20 +10,22 @@ class CHLpredictor(nn.Module):
         self.hidden_size = hidden_size
         self.num_stacked_layers = num_stacked_layers
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_stacked_layers,
+        # self.lstm = nn.LSTM(input_size, hidden_size, num_stacked_layers,
+        #                     batch_first=True)
+        self.model = nn.GRU(input_size, hidden_size, num_stacked_layers,
                             batch_first=True)
-
         self.fc = nn.Linear(hidden_size, out_features)
 
         self.device = device
-        self.lstm.to(self.device)
+        self.model.to(self.device)
 
     def forward(self, x):
         batch_size = x.size(0)
         h0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(self.device)
         c0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(self.device)
 
-        out, _ = self.lstm(x, (h0, c0))
+        #out, _ = self.model(x, (h0, c0))
+        out, _ = self.model(x, h0)
         out = self.fc(out[:, -1, :])
         return out
 
@@ -54,40 +56,66 @@ def prepare_dataframe_for_lstm_with_date(df, n_steps):
     df.dropna(inplace=True)
 
     df1 = dc(df[['Date', 'Close']])
-    df2 = dc(df[['Date', 'Low']])
-    df3 = dc(df[['Date', 'High']])
-    df4 = dc(df[['Date', 'Volume']])
+    df2 = dc(df[['Date', 'Close']])
+    df3 = dc(df[['Date', 'Low']])
+    df4 = dc(df[['Date', 'High']])
+    df5 = dc(df[['Date', 'Volume']])
+
+    df2['CloseChange'] = df1['Close'].pct_change(fill_method='ffill')
+    df3['LowChange'] = df3['Low']/df1['Close'].shift(1)
+    df4['HighChange'] = df4['High']/df1['Close'].shift(1)
+    df1 = df1.shift(1)
+    df5 = df5.shift(1)
+
+    df2.drop(['Close'], axis='columns', inplace=True)
+    df3.drop(['Low'], axis='columns', inplace=True)
+    df4.drop(['High'], axis='columns', inplace=True)
 
     df1.set_index('Date', inplace=True)
     df2.set_index('Date', inplace=True)
     df3.set_index('Date', inplace=True)
     df4.set_index('Date', inplace=True)
+    df5.set_index('Date', inplace=True)
 
     for i in range(1, n_steps + 1):
         df1[f'Close(t-{i})'] = df1['Close'].shift(i)
-        df2[f'Low(t-{i})'] = df2['Low'].shift(i)
-        df3[f'High(t-{i})'] = df3['High'].shift(i)
-        df4[f'Volume(t-{i})'] = df4['Volume'].shift(i)
+        df2[f'CloseChange(t-{i})'] = df2['CloseChange'].shift(i)
+        df3[f'LowChange(t-{i})'] = df3['LowChange'].shift(i)
+        df4[f'HighChange(t-{i})'] = df4['HighChange'].shift(i)
+        df5[f'Volume(t-{i})'] = df5['Volume'].shift(i)
 
     df1.dropna(inplace=True)
     df2.dropna(inplace=True)
     df3.dropna(inplace=True)
     df4.dropna(inplace=True)
+    df5.dropna(inplace=True)
 
-    return df1, df2, df3, df4
+    return df1, df2, df3, df4, df5
 
 def prepare_dataframe_for_lstm(df, n_steps):
     df = dc(df)
 
     df1 = dc(df[['Close']])
-    df2 = dc(df[['Low']])
-    df3 = dc(df[['High']])
-    df4 = dc(df[['Volume']])
+    df2 = dc(df[['Close']])
+    df3 = dc(df[['Low']])
+    df4 = dc(df[['High']])
+    df5 = dc(df[['Volume']])
+
+    df2['CloseChange'] = df1['Close'].pct_change(fill_method='ffill')
+    df3['LowChange'] = df3['Low'] / df1['Close'].shift(1)
+    df4['HighChange'] = df3['High'] / df1['Close'].shift(1)
+    df1 = df1.shift(1)
+    df5 = df5.shift(1)
+
+    df2.drop(['Close'], axis='columns', inplace=True)
+    df3.drop(['Low'], axis='columns', inplace=True)
+    df4.drop(['High'], axis='columns', inplace=True)
 
     for i in range(1, n_steps + 1):
         df1[f'Close(t-{i})'] = df1['Close'].shift(i)
-        df2[f'Low(t-{i})'] = df2['Low'].shift(i)
-        df3[f'High(t-{i})'] = df3['High'].shift(i)
-        df4[f'Volume(t-{i})'] = df4['Volume'].shift(i)
+        df2[f'CloseChange(t-{i})'] = df2['CloseChange'].shift(i)
+        df3[f'LowChange(t-{i})'] = df3['LowChange'].shift(i)
+        df4[f'HighChange(t-{i})'] = df4['HighChange'].shift(i)
+        df5[f'Volume(t-{i})'] = df5['Volume'].shift(i)
 
-    return df1, df2, df3, df4
+    return df1, df2, df3, df4, df5
